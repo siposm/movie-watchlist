eff95843b60cc9edac3d75ab776bd84f42d934e4
Sipos Miklos <commit@siposm.hu>
2026-04-28 17:53:50 +0200

    entity + seed

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Movie } from './movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  movies: Movie[] = []
+
+  constructor() {
+    this.seed()
+  }
+
+  seed(): void {
+    let m: Movie = new Movie()
+    m.title = "Interstellar"
+    m.year = 2014
+    m.watched = false
+    m.rating = 5
+    m.imgUrl = "https://m.media-amazon.com/images/M/MV5BNzllYzMxMzgtODM1YS00OThjLWE0MmUtZjExMDYyYWFhZTgzXkEyXkFqcGc@._V1_.jpg"
+
+    this.movies.push(m)
+
+    m = new Movie()
+    m.title = "Inception"
+    m.year = 2010
+    m.watched = false
+    m.rating = 4
+    m.imgUrl = "https://m.media-amazon.com/images/M/MV5BY2I5NWQxYmUtYTVkYy00YjUwLWEyM2YtNDczNzUxMTM3ZmI2XkEyXkFqcGc@._V1_.jpg"
+
+    this.movies.push(m)
+
+    m = new Movie()
+    m.title = "The Matrix"
+    m.year = 1999
+    m.watched = true
+    m.rating = 5
+    m.imgUrl = "https://m.media-amazon.com/images/M/MV5BZDJkYzdkN2YtZDI1MS00MTE2LWE3OTgtYjcwNzEzYTYxYzhhXkEyXkFqcGc@._V1_.jpg"
+
+    this.movies.push(m)
+  }
 }
--- src/app/movie.ts
+++ src/app/movie.ts
@@ -1,2 +1,10 @@
+import { Guid } from "guid-typescript"
+
 export class Movie {
+    id: string = Guid.create().toString()
+    title: string = ""
+    year: number | null = null
+    watched: boolean = false
+    rating: number | null = null
+    imgUrl: string = ""
 }
